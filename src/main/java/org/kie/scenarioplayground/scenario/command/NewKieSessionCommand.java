bce2ed55aa28ca0e0ab4701621573a4eeff71020
Daniele Zonca <daniele.zonca@gmail.com>
2018-06-15 09:36:27 +0200

    Minor fix

--- src/main/java/org/kie/scenarioplayground/scenario/command/NewKieSessionCommand.java
+++ src/main/java/org/kie/scenarioplayground/scenario/command/NewKieSessionCommand.java
@@ -57,7 +57,7 @@ public class NewKieSessionCommand
         // TODO is it correct to override user defined session with pseudo clock?
         if(getClockTypeOption() != null) {
             final KieSessionModel kieSessionModel = kieContainer.getKieSessionModel(sessionId);
-            kieSessionModel.setClockType(ClockTypeOption.get("pseudo"));
+            kieSessionModel.setClockType(getClockTypeOption());
         }
 
         KieSession ksession  = sessionId != null ? kieContainer.newKieSession(sessionId) : kieContainer.newKieSession();
